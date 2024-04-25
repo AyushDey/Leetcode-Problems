cf8a795dec46dc2a0caf7d79eade32785bd6bc39
AyushDey <ayush.dey@icloud.com>
2024-04-26 01:49:26 +0530

    update L36_validSudoku.py

--- Arrays/L36_validSudoku.py
+++ Arrays/L36_validSudoku.py
@@ -15,20 +15,24 @@ import collections
 def validSudoku(board):
     col = collections.defaultdict(set)
     row = collections.defaultdict(set)
-    squares = collections.defaultdict(
-        set
-    )  # Checking the 3x3 matrices by indexing by (r//3, c//3)
+    # Checking the 3x3 matrices by indexing by (r//3, c//3)
+    squares = collections.defaultdict(set) 
 
     for r in range(9):
         for c in range(9):
+            # Skip empty cells
             if board[r][c] == ".":
                 continue
+            # Check if current number exists in 
+            # corresponding row/col/square
             if (
                 board[r][c] in row[r]
                 or board[r][c] in col[c]
                 or board[r][c] in squares[(r // 3, c // 3)]
             ):
+                # If duplicate found, board is invalid
                 return False
+            # Add current number to row/col/square sets
             col[c].add(board[r][c])
             row[r].add(board[r][c])
             squares[(r // 3, c // 3)].add(board[r][c])
